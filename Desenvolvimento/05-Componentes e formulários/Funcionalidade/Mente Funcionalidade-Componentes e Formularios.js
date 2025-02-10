90030946a83b3bb1577e346105dcb08dec29dae6
Felipe Silva Loschi <166191574+F-Loschi@users.noreply.github.com>
2025-02-10 16:06:41 -0300

    Add files via upload

---
+++ Desenvolvimento/05-Componentes e formulários/Funcionalidade/Mente Funcionalidade-Componentes e Formularios.js
@@ -0,0 +1,27 @@
+function openMenu() {
+    document.getElementById("menu_aba").style.width = "75%";
+}
+
+function closeMenu() {
+    document.getElementById("menu_aba").style.width = "0";
+}
+
+function openProfile() {
+    document.getElementById("profile_aba").style.width = "75%";
+}
+
+function closeProfile() {
+    document.getElementById("profile_aba").style.width = "0";
+}
+
+function goBack() {
+    // Ação para o botão de seta para trás
+    console.log("Botão de seta para trás clicado");
+    // Insira aqui o código para a ação desejada
+}
+
+function goForward() {
+    // Ação para o botão de seta para frente
+    console.log("Botão de seta para frente clicado");
+    // Insira aqui o código para a ação desejada
+}
